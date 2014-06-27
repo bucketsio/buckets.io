ba457881e126695b429479f2b37945809e35bcb4
David Kaneda <dk@morfunk.com>
2014-06-27 02:27:11 -0700

    Style updates
    Updates copy, adds animations via GSAP, clickable logo. Still needs to minify js.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/**
+ * author Christopher Blum
+ *    - based on the idea of Remy Sharp, http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+ *    - forked from http://github.com/zuk/jquery.inview/
+ */
 (function ($) {
   var inviewObjects = {}, viewportSize, viewportOffset,
       d = document, w = window, documentElement = d.documentElement, expando = $.expando, timer;
